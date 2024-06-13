09092dfdb44986a958373aecdf2892cfd076c4be
heyairf <heyairf@gmail.com>
2024-06-13 17:57:44 -0600

    added self contained example data

---
+++ inst/extdata/exampledatageneration.R
@@ -0,0 +1,20 @@
+# generate example hazard data -----------------------------
+set.seed(0)
+e <- c(45,55,495,505) * 10000
+r <- terra::rast(resolution = 100, extent = terra::ext(e))
+terra::values(r) <- sample(c(0,1), terra::ncell(r), replace = TRUE)
+terra::crs(r) <- "EPSG:32608"
+r <- terra::sieve(r, threshold = 50, directions = 4)
+haz <- terra::sieve(r, threshold = 500, directions = 4)
+# generate an example point ---------------------------------
+wkt <- "POINT (500000 5000000)"
+pt <- terra::vect(wkt, crs = haz)
+# generate example AOI polygon -----------------------------
+filepath <- "extdata/builtsimpleexamplegeom.csv"
+g <- read.csv(system.file(filepath, package = "fireexposuR"))
+m <- as.matrix(g)
+v <- terra::vect(m, "polygons", crs = haz)
+# generate example point values within polygon -------------
+pts <- terra::spatSample(v, 200)
+# ----------------------------------------------------------
+
